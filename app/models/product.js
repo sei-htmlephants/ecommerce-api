744ef175b22ac5c9d190171fe01be7e0139d8d34
sidhantmathur <sidhant185@gmail.com>
2020-11-29 14:29:49 -0600

    init product schema, add curl scripts

---
+++ app/models/product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true
+  },
+  productDescription: {
+    type: String,
+    required: true
+  },
+  productPrice: {
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  productClass: {
+    type: String,
+    enum: ['official', 'market']
+    // need to seperate marketplace and e-commerce products
+  },
+  productCatagory: {
+    type: String,
+    enum: ['Cameras', 'Prints', 'Accessories']
+  },
+  productImages: {
+    type: String
+    // will be image links?
+    // take first image as profile, or make another catagory?
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Product', productSchema)
